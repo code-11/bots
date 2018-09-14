093512e309accf6e799010c2b38e469ac505fce2
code-11 <brendan.ritter@yahoo.com>
2018-09-13 23:43:15 -0400

    Made test.py into a Game class

--- src/test.py
+++ src/test.py
@@ -6,73 +6,81 @@ from Players import *
 
 import sys   # for exit and arg
 
+class Game:
+	players=None
+	board=None
+	done = False
 
-def Draw(surf,board,players):
-  #Clear view
-  surf.fill((200,200,200))
+	def __init__(self):
+		self.players=Players()
+		self.board=Board()
 
-  board_painter=board.make_painter()
-  board_painter(surf,players.color_map)
+	def Draw(self,surf):
+		#Clear view
+		surf.fill((200,200,200))
 
-  players_painter=players.make_painter()
-  players_painter(surf)
+		board_painter=self.board.make_painter()
+		board_painter(surf,self.players.color_map)
 
-  pygame.display.flip()
+		players_painter=self.players.make_painter()
+		players_painter(surf)
 
-
-def GetInput(board,players):
-
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      return True
-    if event.type == KEYDOWN or event.type == MOUSEBUTTONDOWN:
-      print (event)
-      players.move(board)
-    sys.stdout.flush()  # get stuff to the console
-  return False
+		pygame.display.flip()
 
 
-Done = False
+	def GetInput(self,board,players):
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				return True
+			if event.type == KEYDOWN or event.type == MOUSEBUTTONDOWN:
+				print (event)
+				self.players.move(board)
+			sys.stdout.flush()  # get stuff to the console
+		return False
 
-def quit_callback():
-  global Done
-  Done = True
+	def quit_callback(self):
+		self.done = True
 
-def main(board,players):
+	def main(self):
+		# initialise pygame
+		pygame.init()
+		ScreenSize = self.board.board_size()
+		Surface = pygame.display.set_mode(ScreenSize)
 
-  # initialise pygame
-  pygame.init()
-  ScreenSize = board.board_size()
-  Surface = pygame.display.set_mode(ScreenSize)
+		#initialise tkinter
+		root = tkinter.Tk()
+		root.protocol("WM_DELETE_WINDOW", self.quit_callback)
+		main_dialog =  tkinter.Frame(root)
+		main_dialog.winfo_toplevel().title("Bot Paint Arena")
+		main_dialog.pack()
 
-  #initialise tkinter
-  root = tkinter.Tk()
-  root.protocol("WM_DELETE_WINDOW", quit_callback)
-  main_dialog =  tkinter.Frame(root)
-  main_dialog.pack()
+		player_lbl = tkinter.Label(root,text="Number Players Loaded: "+str(self.players.num_players()))
+		player_lbl.pack()
 
-  # start pygame clock
-  clock = pygame.time.Clock()
-  gameframe = 0
+		b = tkinter.Button(root, text="OK") 
+		b.pack()
 
-  # main loop
-  while not Done:
-    try:
-      main_dialog.update()
-    except:
-      print ("dialog error")
+		# start pygame clock
+		clock = pygame.time.Clock()
+		gameframe = 0
 
-    if GetInput(board,players):  # input event can also comes from diaglog
-      break
-    Draw(Surface,board,players)
-    clock.tick(100) # slow it to something slightly realistic
-    gameframe += 1
+	  # main loop
+		while not self.done:
+			try:
+				main_dialog.update()
+			except:
+				print ("dialog error")
 
-  main_dialog.destroy()
+			if self.GetInput(self.board,self.players):  # input event can also comes from diaglog
+			  break
+			self.Draw(Surface)
+			clock.tick(100) # slow it to something slightly realistic
+			gameframe += 1
+
+		main_dialog.destroy()
 
 
 if __name__ == '__main__': 
 	# load_players()
-	b=Board()
-	ps=Players()
-	main(b,ps)
+	game=Game()
+	game.main()
